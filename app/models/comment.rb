d6cb6116e860e6986fb8e5472b45128c2e415164
Shuo Zhang <shuo.zhang.us@gmail.com>
2014-03-17 18:30:58 -0400

    added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+  validates :content, length: {within: 5..150}
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :destroy
+  validates :title, presence: true
+  validates :title, uniqueness: true
 
   #attr_accessible :name, :start_time
   #def start_time
