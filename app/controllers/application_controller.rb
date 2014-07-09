ea06aebd3700610a1361a55488f523c1bca9c8d6
Shuo Zhang <shuo.zhang.us@gmail.com>
2014-07-09 15:32:54 -0400

    showabout edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :authenticate_user!, :except => [:userindex, :showabout]
+  before_filter :authenticate_user!, :except => [:showabout]
   before_filter :update_sanitized_params, if: :devise_controller?
 
   def search
