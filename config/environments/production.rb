5ab91de0fcf8f5406a10422dde763824ac28858f
Shuo Zhang <shuo.zhang.us@gmail.com>
2014-08-01 01:07:32 -0400

    delivery fix 11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,13 @@ SocialCalendar3::Application.configure do
   config.filepicker_rails.cdn_host = "d2v8blf6aa0zzy.cloudfront.net"
   
   config.action_mailer.default_url_options = {host: 'penncommunity.com'}
-  config.action_mailer.delivery_method = :sendmail
-  #config.action_mailer.smtp_settings = {
-  #  :address => 'smtp.sendgrid.net',
-  #  :port => '587',
-  #  :user_name => ENV['SENDGRID_USERNAME'],
-  #  :password => ENV['SENDGRID_PASSWORD'],
-  #  :domain => ENV['SENDGRID_DOMAIN']
-  #}
+  #config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :user_name => "app23110105@heroku.com",
+    :password => "zutqyfmt",
+    :domain => "heroku.com"
+  }
 
 end
