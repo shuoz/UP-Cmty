864718349aae0f06cf5261bd642b2bf63f19fb2f
Shuo Zhang <shuo.zhang.us@gmail.com>
2014-08-05 17:48:01 -0400

    mailer from admin

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,5 +1,5 @@
 class Notifications < ActionMailer::Base
-  default from: "shuoz@wharton.upenn.edu"
+  default from: "admin@penncommunity.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
